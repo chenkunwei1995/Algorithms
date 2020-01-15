facace7dce21388279a4d11c57f9fc39f9881a78
chenkunwei1995 <m17854225838@163.com>
2020-01-15 23:25:27 +0800

    位信息

---
+++ JavaBasicDev/src/com/java/thread/model/location/Location.java
@@ -0,0 +1,26 @@
+package com.java.thread.model.location;
+
+public class Location {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setXY(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+}
